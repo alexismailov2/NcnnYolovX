7a334b5ee80d9d107b39131c0efcc12e55e4fcf9
Alexander Ismailov <tama.rilla@protonmail.com>
2023-03-23 16:39:04 +0100

    Moved to bcc32c from bcc32x.
    Signed-off-by: Alexander Ismailov <tama.rilla@protonmail.com>

--- build_bcc32.sh
+++ build_bcc32.sh
@@ -14,8 +14,8 @@ cd ncnn
 cmake -Bbuild_embarc\
  -G "Borland Makefiles"\
  -DCMAKE_MAKE_PROGRAM="/c/Program Files (x86)/Embarcadero/Studio/21.0/bin/make.exe"\
- -DCMAKE_C_COMPILER=bcc32x.exe\
- -DCMAKE_CXX_COMPILER=bcc32x.exe\
+ -DCMAKE_C_COMPILER=bcc32c.exe\
+ -DCMAKE_CXX_COMPILER=bcc32c.exe\
  -DCMAKE_BUILD_TYPE_INIT=Debug\
  -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON\
  -DCMAKE_C_COMPILER_WORKS=ON\
@@ -34,14 +34,21 @@ cmake -Bbuild_embarc\
  -DNCNN_INSTALL_SDK=ON
 cp ../patches/cpuid.h ./src/
 sed -i 's:/:\\:g' ./build_embarc/src/CMakeFiles/ncnn.dir/objects1.rsp
+sed -i 's:-Wall: :g' "./build_embarc/src/CMakeFiles/ncnn.dir/flags.make"
+sed -i 's:-Wextra: :g' "./build_embarc/src/CMakeFiles/ncnn.dir/flags.make"
+sed -i 's:-Wno-unused-function: :g' "./build_embarc/src/CMakeFiles/ncnn.dir/flags.make"
+sed -i 's:-Ofast: :g' "./build_embarc/src/CMakeFiles/ncnn.dir/flags.make"
+sed -i 's:-fvisibility=hidden: :g' "./build_embarc/src/CMakeFiles/ncnn.dir/flags.make"
+sed -i 's:-fvisibility-inlines-hidden: :g' "./build_embarc/src/CMakeFiles/ncnn.dir/flags.make"
+
 cmake --build build_embarc --target install
 cd ..
 
 cmake -Bbuild_bcc32\
  -G "Borland Makefiles"\
  -DCMAKE_MAKE_PROGRAM="/c/Program Files (x86)/Embarcadero/Studio/21.0/bin/make.exe"\
- -DCMAKE_C_COMPILER=bcc32x.exe\
- -DCMAKE_CXX_COMPILER=bcc32x.exe\
+ -DCMAKE_C_COMPILER=bcc32c.exe\
+ -DCMAKE_CXX_COMPILER=bcc32c.exe\
  -DCMAKE_BUILD_TYPE=$build_type\
  -DCMAKE_VERBOSE_MAKEFILE=ON\
  -DCMAKE_C_COMPILER_WORKS=ON\
@@ -55,4 +62,4 @@ sed -i 's:-Od: :g' "./build_bcc32/test/CMakeFiles/NcnnYolov8_test1.dir/flags.mak
 sed -i 's:-Od: :g' "./build_bcc32/test/CMakeFiles/NcnnYolov8_test1.dir/build.make"
 cmake --build build_bcc32 --target install
 
-./build_bcc32/test/NcnnYolov8Test1.cpp.exe ./assets/parking.jpg ./assets/yolov8s.param ./assets/yolov8s.bin ./assets/yolov8s.classes 640
+./build_bcc32/test/NcnnYolov8_test1.exe ./assets/parking.jpg ./assets/yolov8s.param ./assets/yolov8s.bin ./assets/yolov8s.classes 640

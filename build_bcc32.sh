5c77a78d25b8f9c49d1e63ec896ca0aec4922813
Alexander Ismailov <tama.rilla@protonmail.com>
2023-03-23 02:30:10 +0100

    Added fixes for ability to put NCNN_ROOT path directly due to workarounds around fixing build scripts for Embarcadero compiler.
    Signed-off-by: Alexander Ismailov <tama.rilla@protonmail.com>

--- build_bcc32.sh
+++ build_bcc32.sh
@@ -8,6 +8,34 @@ else
   echo "build_type='$build_type'";
 fi
 
+git clone https://github.com/Tencent/ncnn.git
+cd ncnn
+
+cmake -Bbuild_embarc\
+ -G "Borland Makefiles"\
+ -DCMAKE_MAKE_PROGRAM="/c/Program Files (x86)/Embarcadero/Studio/21.0/bin/make.exe"\
+ -DCMAKE_C_COMPILER=bcc32x.exe\
+ -DCMAKE_CXX_COMPILER=bcc32x.exe\
+ -DCMAKE_BUILD_TYPE_INIT=Debug\
+ -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON\
+ -DCMAKE_C_COMPILER_WORKS=ON\
+ -DCMAKE_CXX_COMPILER_WORKS=ON\
+ -DNCNN_BUILD_TOOLS=OFF\
+ -DNCNN_BUILD_EXAMPLES=OFF\
+ -DNCNN_BUILD_BENCHMARK=OFF\
+ -DNCNN_AVX=OFF\
+ -DNCNN_AVX2=OFF\
+ -DNCNN_SSE2=OFF\
+ -DNCNN_RUNTIME_CPU=OFF\
+ -DNCNN_THREADS=OFF\
+ -DNCNN_OPENMP=OFF\
+ -DNCNN_XOP=OFF\
+ -DNCNN_PIXEL_AFFINE=OFF\
+ -DNCNN_INSTALL_SDK=ON
+cp ../cpuid.h ./src/
+sed -i 's:/:\\:g' ./build_embarc/src/CMakeFiles/ncnn.dir/objects1.rsp
+cmake --build build_embarc --target install
+
 cmake -Bbuild_bcc32\
  -G "Borland Makefiles"\
  -DCMAKE_MAKE_PROGRAM="/c/Program Files (x86)/Embarcadero/Studio/21.0/bin/make.exe"\
@@ -16,7 +44,8 @@ cmake -Bbuild_bcc32\
  -DCMAKE_BUILD_TYPE=$build_type\
  -DCMAKE_VERBOSE_MAKEFILE=ON\
  -DCMAKE_C_COMPILER_WORKS=ON\
- -DCMAKE_CXX_COMPILER_WORKS=ON
+ -DCMAKE_CXX_COMPILER_WORKS=ON\
+ -DNCNN_ROOT=./build_embarc/install
 #should be moved to ExternalAdd project
 #cp ../cpuid.h ./src/
 #sed -i 's:/:\\:g' ./build_bcc32/src/CMakeFiles/ncnn.dir/objects1.rsp
--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,12 +1,15 @@
+include(ExternalProject)
+
 if("${DEPENDENCIES_PREFIX_PATH}" STREQUAL "")
     set(DEPENDENCIES_PREFIX_PATH ${CMAKE_SOURCE_DIR}/OIYoloDependencies)
 endif()
 message(STATUS DEPENDENCIES_PREFIX_PATH=${DEPENDENCIES_PREFIX_PATH})
 
+if("${DEPENDENCIES_PREFIX_PATH}" STREQUAL "")
 if(EXISTS ${DEPENDENCIES_PREFIX_PATH}/libncnn)
     set(NCNN_ROOT ${DEPENDENCIES_PREFIX_PATH}/libncnn)
     message(STATUS NCNN_ROOT=${NCNN_ROOT})
+else()
+    include(cmake/ExternalNcnn.cmake)
 endif()
-
-include(ExternalProject)
-include(cmake/ExternalNcnn.cmake)
+endif()
